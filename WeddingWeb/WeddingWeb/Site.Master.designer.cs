07a0ca5b814ac70a8eecc0a406251628b8b44a8a
Krikor Nigoghosian Junior <krikor.ni@gmail.com>
2014-01-19 20:58:04 -0800

    MobileMenu

--- WeddingWeb/WeddingWeb/Site.Master.designer.cs
+++ WeddingWeb/WeddingWeb/Site.Master.designer.cs
@@ -31,6 +31,15 @@ namespace WeddingWeb {
         protected global::System.Web.UI.WebControls.Menu NavigationMenu;
         
         /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
